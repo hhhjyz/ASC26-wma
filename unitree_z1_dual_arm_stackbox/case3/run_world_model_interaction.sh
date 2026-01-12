3549dfde298a94c2df877974a79d7a5f16566e37
ASC-Competition <50946625+ASC-Competition@users.noreply.github.com>
2026-01-12 14:05:26 +0800

    Add files via upload

---
+++ unitree_z1_dual_arm_stackbox/case3/run_world_model_interaction.sh
@@ -0,0 +1,24 @@
+res_dir="unitree_z1_dual_arm_stackbox/case3"
+dataset="unitree_z1_dual_arm_stackbox"
+
+{
+    time CUDA_VISIBLE_DEVICES=0 python3 scripts/evaluation/world_model_interaction.py \
+        --seed 123 \
+        --ckpt_path ckpts/unifolm_wma_dual.ckpt \
+        --config configs/inference/world_model_interaction.yaml \
+        --savedir "${res_dir}/output" \
+        --bs 1 --height 320 --width 512 \
+        --unconditional_guidance_scale 1.0 \
+        --ddim_steps 50 \
+        --ddim_eta 1.0 \
+        --prompt_dir "unitree_z1_dual_arm_stackbox/case3/world_model_interaction_prompts" \
+        --dataset ${dataset} \
+        --video_length 16 \
+        --frame_stride 4 \
+        --n_action_steps 16 \
+        --exe_steps 16 \
+        --n_iter 7 \
+        --timestep_spacing 'uniform_trailing' \
+        --guidance_rescale 0.7 \
+        --perframe_ae
+} 2>&1 | tee "${res_dir}/output.log"
